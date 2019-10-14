f07f4833e4c8879580ec29ab0d167aa30e2222d6
一一秋风 <491883290@qq.com>
2019-10-14 09:09:38 +0800

    @

--- react性能优化的一些思考/demo/src/App.js
+++ react性能优化的一些思考/demo/src/App.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      num:1,
+      title:'title'
+    }
+    this.handleClick=this.handleClick.bind(this)
+    this.handleTitle=this.handleTitle.bind(this)
+  }
+  handleClick(){
+    console.log('xx')
+    this.setState({
+      num:this.state.num+1
+    })
+  }
+
+  handleTitle(){
+    console.log('yy')
+    this.setState({
+      title:this.state.title+'!'
+    })
+  }
   render() {
+
+   
+    //每次render  bind都会执行一次
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>App{this.state.num}</h2>
+        <button onClick={this.handleClick}>点击num</button>
+        <button onClick={this.handleTitle}>点击title</button>
+        {/*<button onClick={()=>this.handleClick()}>点击2</button>*/}
+        <Demo title={this.state.title}></Demo>
       </div>
     );
+  }
+}
+
+class Demo extends React.PureComponent{
+   
+  render(){
+    console.log('demo render执行中')
+    return (
+      <h2>I am Demo,title:{this.props.title}</h2>
+    )
+
+
   }
 }
 
