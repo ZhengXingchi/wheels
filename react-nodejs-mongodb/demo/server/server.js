3a9bd5c711c00ca18ca28977ab6a87f38cc71287
一一秋风 <491883290@qq.com>
2019-10-14 14:07:00 +0800

    服务器运行

--- react-nodejs-mongodb/demo/server/server.js
+++ react-nodejs-mongodb/demo/server/server.js
@@ -5,7 +5,7 @@ const cookieParser=require('cookie-parser')
 const userRouter=require('./user')
 const model=require('./model')
 const Chat=model.getModel('chat')
-
+const path=require('path')
 
 const app =express()
 
@@ -31,8 +31,45 @@ io.on('connection',(socket)=>{
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use('/user',userRouter)
-
+app.use((req,res,next)=>{
+  if(req.url.startsWith('/user/')||req.url.startsWith('/static')){
+    return next()
+  }else{
+    console.log('path resolve',path.resolve('build/index.html'))
+    return res.sendFile(path.resolve('build/index.html'))
+  }
+})
+app.use('/',express.static(path.resolve('build')))
 
 server.listen(9093,()=>{
   console.log('Node app start at port 9093')
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- react性能优化的一些思考/demo/src/App.js
+++ react性能优化的一些思考/demo/src/App.js
@@ -1,22 +1,79 @@
 import React, { Component } from 'react';
-
+import {
+  Map,
+  is
+}from 'immutable'
 import './App.css';
+import _  from 'lodash';
 
+
+// function compareObj(obj1,obj2){
+//   if(obj1==obj2){
+//     return true
+//   }
+//   if(Object.keys(obj1).length!==Object.keys(obj2).length){
+//     return false
+//   }
+//   for(let k in obj1){
+//     if(typeof obj1[k]=='object'){
+     
+//       if(!compareObj(obj1[k],obj2[k])){
+//         return false
+//       }
+//     }else if (obj1[k]!==obj2[k]){
+     
+//       return false
+      
+//     }
+//   }
+//   return true
+// }
+
+let obj1=Map({
+  'name':'haha',
+  'course':Map({name:'react+redux'})
+})
+let obj2=obj1.set('name','hahala')
+
+
+
+
+
+console.log('obj1==obj2',obj1.get('course')==obj2.get('course'))
 class App extends Component {
   constructor(props){
     super(props)
     this.state={
       num:1,
-      title:'title'
+      title:'title',
+      map:['aa','bb','cc']
     }
     this.handleClick=this.handleClick.bind(this)
     this.handleTitle=this.handleTitle.bind(this)
+    this.handleMap=this.handleMap.bind(this)
+
+
   }
   handleClick(){
     console.log('xx')
     this.setState({
       num:this.state.num+1
     })
+  }
+
+  handleMap(){
+     this.setState({
+      num:this.state.num+1
+    },()=>{
+    console.log(this.state.num)
+      let arr=_.cloneDeep(this.state.map)
+      arr.unshift(this.state.num)
+      this.setState({
+        map: arr
+      })
+    })
+
+    
   }
 
   handleTitle(){
@@ -27,15 +84,23 @@ class App extends Component {
   }
   render() {
 
-   
+    const {map}=this.state
     //每次render  bind都会执行一次
     return (
       <div>
         <h2>App{this.state.num}</h2>
         <button onClick={this.handleClick}>点击num</button>
         <button onClick={this.handleTitle}>点击title</button>
+        <button onClick={this.handleMap}>点击map</button>
         {/*<button onClick={()=>this.handleClick()}>点击2</button>*/}
         <Demo title={this.state.title}></Demo>
+        {map.map((item,i)=>{
+          return(
+            
+               <div key={i}>{item}</div>
+              
+          )
+        })}
       </div>
     );
   }
